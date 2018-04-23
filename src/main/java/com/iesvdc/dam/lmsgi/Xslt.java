6b12a16fe7821185b06e587c5188c87d4d48786f
Juan Gualberto <jgutierrez@iesvirgendelcarmen.com>
2018-04-23 09:17:55 +0200

    first commit

---
+++ src/main/java/com/iesvdc/dam/lmsgi/Xslt.java
@@ -0,0 +1,13 @@
+package com.iesvdc.dam.lmsgi;
+
+/**
+ * Hello world!
+ *
+ */
+public class Xslt 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
